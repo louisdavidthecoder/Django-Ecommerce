e27f2f5449248642a402a43545c0ec4660069364
Louis David <louisdavid7070@.com>
2021-04-23 11:19:32 -0400

    2

---
+++ basket/basket.py
@@ -0,0 +1,43 @@
+from decimal import Decimal
+from store.models import Product
+
+
+class Basket():
+    """
+    A base Basket class, providing some default behaviors that
+    can be inherited or overrided, as necessary.
+    """
+
+    def __init__(self, request):
+        self.session = request.session
+        basket = self.session.get('skey')
+        if 'skey' not in request.session:
+            basket = self.session['skey'] = {}
+        self.basket = basket    
+
+    def add(self, product, qty):
+        
+        product_id = product.id
+        if product_id not in self.basket:   
+            self.basket[product_id] = {'price': str(product.price), 'qty':int(qty) }
+
+        self.session.modified = True   
+
+    def __iter__(self):
+        product_ids = self.basket.keys()
+        products = Product.products.filter(id__in=product_ids)
+        basket =self.basket.copy()
+
+        for product in products:
+            basket[str(product.id)]['product'] = product
+
+        for item in basket.values():
+            item['price'] = Decimal(item['price'])  
+            item['total_price'] = item['price'] * item['qty'] 
+            yield item 
+
+    def __len__(self):
+        return sum(item['qty'] for item in self.basket.values())    
+
+    def get_total_price(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.basket.values())    
